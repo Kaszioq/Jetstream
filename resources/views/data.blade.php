fc3ee9610a2f0e4d9319854d778c9c8dbc7e173d
Kaszioq <kubakaszewskisl@gmail.com>
2023-10-18 13:53:49 +0200

    fix

---
+++ resources/views/data.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Dane użytkownika</h3>
+    Imię i nazwisko: {{$firstName}} {{$lastName}}<br>
+    Miasto: {{$city}}
+</body>
+</html>
